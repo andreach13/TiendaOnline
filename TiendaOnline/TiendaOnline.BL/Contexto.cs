95ddb6f48393e2b998e2f077e167d2cea03d2b3c
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-06-17 20:42:59 -0600

    Conectando BD

---
+++ TiendaOnline/TiendaOnline.BL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiendaOnline.BL
+{
+    class Contexto: DbContext
+    {
+        public Contexto(): base("TiendaOnlineBD")
+        {
+
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+    }
+}
