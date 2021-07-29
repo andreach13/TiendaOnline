cfb0f8e59d58afc2bf523652ebe222eb779f0999
Andrea Chavarria <andrea.chavarria@unah.hn>
2021-07-28 21:50:28 -0600

    Agregando plantilla a sitio web publico

--- TiendaOnline/TiendaOnline.Web/Controllers/HomeController.cs
+++ TiendaOnline/TiendaOnline.Web/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TiendaOnline.BL;
 
 namespace TiendaOnline.Web.Controllers
 {
@@ -11,7 +13,14 @@ namespace TiendaOnline.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+
+
+            return View(listadeproductos);
         }
     }
 }
